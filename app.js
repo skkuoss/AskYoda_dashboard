4da8625ccda6307dbb3f2b65b39742f8dc077737
borquezz <jesusfernandoborquez@gmail.com>
2021-11-27 18:56:13 +0900

    input autocomplete

--- app.js
+++ app.js
@@ -35,17 +35,58 @@ data = await response.json();
 const loadMusic = async () => {
     song1 = document.getElementById("song1").value;
     song2 = document.getElementById("song2").value;
-    if (song1 === "" || song2 === "")
-        return;
+    if (song1 === "" || song2 === "") return;
 
     let response = await fetch(
-            `http://localhost:3100/v1/recommendations?songs=${song1}&songs=${song2}`
-        );
+        `http://localhost:3100/v1/recommendations?songs=${song1}&songs=${song2}`
+    );
     data = await response.json();
 };
 
 // document.getElementById("askYodaBtn").addEventListener("mouseover", loadMusic);
 
+document.getElementById("song1").addEventListener("keyup", async (e) => {
+    // If the key is not defined, don't show any autocomplete
+    if (!e.key) {
+        return;
+    }
+    let song = document.getElementById("song1");
+    let songList1 = document.getElementById("songList1");
+    let response = await fetch(
+        `http://localhost:3100/v1/autocomplete?trackName=${song.value}`
+    );
+    data = await response.json();
+    songList1.innerHTML = "";
+    for (let index = 0; index < 5; index++) {
+        let songOption = document.createElement("option");
+        songOption.setAttribute(
+            "value",
+            `${data.body.tracks.items[index].name} - ${data.body.tracks.items[index].artists[0].name}`
+        );
+        songList1.appendChild(songOption);
+    }
+});
+document.getElementById("song2").addEventListener("keyup", async (e) => {
+    // If the key is not defined, don't show any autocomplete
+    if (!e.key) {
+        return;
+    }
+    let song = document.getElementById("song2");
+    let songList2 = document.getElementById("songList2");
+    let response = await fetch(
+        `http://localhost:3100/v1/autocomplete?trackName=${song.value}`
+    );
+    data = await response.json();
+    songList2.innerHTML = "";
+    for (let index = 0; index < 5; index++) {
+        let songOption = document.createElement("option");
+        songOption.setAttribute(
+            "value",
+            `${data.body.tracks.items[index].name} - ${data.body.tracks.items[index].artists[0].name}`
+        );
+        songList2.appendChild(songOption);
+    }
+});
 
 document.getElementById("askYodaBtn").onclick = async () => {
     document.getElementById("resultsContainer").innerHTML = "";
@@ -54,19 +95,23 @@ document.getElementById("askYodaBtn").onclick = async () => {
     song2 = document.getElementById("song2").value;
 
     if (song1 === "" || song2 === "") {
-        document.getElementById("lukeBubble").innerHTML = `Please fill both songs!`;
-        document.getElementById("lukeBubble").style.fontWeight = 'bold';
+        document.getElementById(
+            "lukeBubble"
+        ).innerHTML = `Please fill both songs!`;
+        document.getElementById("lukeBubble").style.fontWeight = "bold";
         return;
     }
-
-    document.getElementById("lukeBubble").innerHTML = `I like ${song1} and ${song2}.`;
-    document.getElementById("lukeBubble").style.fontWeight = 'normal';
 
     let response = await fetch(
         `http://localhost:3100/v1/recommendations?songs=${song1}&songs=${song2}`
     );
     data = await response.json();
 
+    document.getElementById("lukeBubble").innerHTML = `I like ${song1.replace(
+        "-",
+        "by"
+    )} and ${song2.replace("-", "by")}.`;
+    document.getElementById("lukeBubble").style.fontWeight = "normal";
     // dark theme
     // document.getElementById("header").classList.add("darkHeader");
     // document.getElementById("mainContainer").style.display = "none";
@@ -75,8 +120,8 @@ document.getElementById("askYodaBtn").onclick = async () => {
 
     // console.log(data.tracks);
 
-    data.tracks.forEach(tableRow => {
-        console.log(tableRow.album.images[0]);
+    data.tracks.forEach((tableRow) => {
+        // console.log(tableRow.album.images[0]);
 
         /* <div class="card">
             <img src="./images/yodaIcon.png" alt="">
@@ -105,7 +150,10 @@ document.getElementById("askYodaBtn").onclick = async () => {
 
 document.getElementById("yodaIcon").style.transform = "translateY(-20px)";
 
-document.getElementById("askYodaBtn").addEventListener("mouseover", () =>
-    // document.getElementById("yodaIcon")
-    document.getElementById("yodaIcon").style.transform = "translateY(-20px)"
+document.getElementById("askYodaBtn").addEventListener(
+    "mouseover",
+    () =>
+        // document.getElementById("yodaIcon")
+        (document.getElementById("yodaIcon").style.transform =
+            "translateY(-20px)")
 );
